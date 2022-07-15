00b6144d96eb03f2dcf70affe0a35ee32743a176
Dave Toerber <d.toerber@marrickdevelopmen.com>
2022-07-15 11:17:27 -0600

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FlexComponent } from './flex/flex.component';
+import { GridComponent } from './grid/grid.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/grid', pathMatch: 'full' },
+  { path: 'grid', component: GridComponent },
+  { path: 'flex', component: FlexComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
